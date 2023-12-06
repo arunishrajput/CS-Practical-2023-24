989447b509ab5053be5f68c2b057149f21e17a0b
Arunish Rajput <arunishrajput7@gmail.com>
2023-12-06 22:33:57 +0530

    code modified

--- 06. Find & Replace Words in txt File.py
+++ 06. Find & Replace Words in txt File.py
@@ -1,13 +1,13 @@
-file = open('test.txt','r+')
+file = open('test.txt','r')
 find = input('Enter word to find:')
 content = file.read()
 
-if find not in content:
+if find not in content.split():
     print('Find word', find, 'not found')
 else:
     replace = input('Enter word to replace with:')
     replaced_content = content.replace(find,replace)
-    file.seek(0)
-    file.write(replaced_content)
-    print('Replaced successfully!')
+    with open('test.txt','w') as file:
+        file.write(replaced_content)
+        print('Replaced successfully!')
 file.close()
