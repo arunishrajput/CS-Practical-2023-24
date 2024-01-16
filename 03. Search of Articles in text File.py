7a77b4dfe56a2ea5cbbef191d3fd68feeb7dabfb
Arunish Rajput <arunishrajput7@gmail.com>
2024-01-16 21:27:20 +0530

    made code efficient

---
+++ 03. Search of Articles in text File.py
@@ -0,0 +1,16 @@
+file = open('test.txt', 'r')
+line = file.readline().lower()
+a = an = the = 0
+while line:
+    for word in line.split():
+        if word == 'a':
+            a += 1
+        elif word == 'an':
+            an += 1
+        elif word == 'the':
+            the += 1
+    line = file.readline().lower()
+print('Total no. of a:',a)
+print('Total no. of an:',an)
+print('Total no. of the:',the)
+file.close()
