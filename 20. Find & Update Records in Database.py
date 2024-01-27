0cb22337d49670eb4a608d330c19274961512358
Arunish Rajput <arunishrajput7@gmail.com>
2024-01-27 22:32:29 +0530

    renamed

---
+++ 20. Find & Update Records in Database.py
@@ -0,0 +1,38 @@
+'''
+Write a python program to update the address of student in student table of Test database.
+Program will search the record by the input roll no. of student. If record not found,
+then display appropriate message.
+
+Assume the following structure of student table:-
+    +-------------------+-----------+------+
+    | Field             | Data Type | Size |
+    +-------------------+-----------+------+
+    | Roll(Primary Key) | INT       |  04  |
+    | Name              | VARCHAR   |  30  |
+    | Address           | CHAR      |  40  |
+    | Phone_no          | VARCHAR   |      |
+    +-------------------+-----------+------+
+'''
+#############################################################################
+
+import mysql.connector as mc
+
+con = mc.connect(host = 'localhost', user = 'root', password = 'password', database = 'Test')
+cur = con.cursor()
+
+Roll = int(input('Enter Roll no. to update address: '))
+
+sql = f"SELECT * FROM student WHERE Roll = {Roll}"
+cur.execute(sql)
+
+if cur.fetchone():
+    address = input('Enter new address: ')
+    sql = f"UPDATE student SET Address = '{address}' WHERE Roll = {Roll}"
+    cur.execute(sql)
+    con.commit()
+    print('Address Updated Successfully!')
+else:
+    print('Record not found!')
+
+cur.close()
+con.close()
